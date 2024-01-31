bc73697168437588d9a5b08b3446bdcb39ac9e82
saidmurod <umarov.s.f@gmail.com>
2024-01-31 17:19:45 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Test2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddCSV = new System.Windows.Forms.Button();
+            this.Count = new System.Windows.Forms.Label();
+            this.Generate = new System.Windows.Forms.Button();
+            this.CountTitle = new System.Windows.Forms.Label();
+            this.AddXML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddCSV
+            // 
+            this.AddCSV.Location = new System.Drawing.Point(100, 120);
+            this.AddCSV.Name = "AddCSV";
+            this.AddCSV.Size = new System.Drawing.Size(400, 30);
+            this.AddCSV.TabIndex = 0;
+            this.AddCSV.Text = "Добавить CSV файл";
+            this.AddCSV.UseVisualStyleBackColor = true;
+            this.AddCSV.Click += new System.EventHandler(this.AddCSV_Click);
+            // 
+            // Count
+            // 
+            this.Count.AutoSize = true;
+            this.Count.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Count.Location = new System.Drawing.Point(483, 235);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(18, 20);
+            this.Count.TabIndex = 1;
+            this.Count.Text = "0";
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(100, 350);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(400, 30);
+            this.Generate.TabIndex = 3;
+            this.Generate.Text = "Сгенерировать";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // CountTitle
+            // 
+            this.CountTitle.AutoSize = true;
+            this.CountTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CountTitle.Location = new System.Drawing.Point(96, 235);
+            this.CountTitle.Name = "CountTitle";
+            this.CountTitle.Size = new System.Drawing.Size(267, 20);
+            this.CountTitle.TabIndex = 5;
+            this.CountTitle.Text = "Записей будет сформирована";
+            // 
+            // AddXML
+            // 
+            this.AddXML.Location = new System.Drawing.Point(100, 80);
+            this.AddXML.Name = "AddXML";
+            this.AddXML.Size = new System.Drawing.Size(400, 30);
+            this.AddXML.TabIndex = 6;
+            this.AddXML.Text = "Добавить XML файл";
+            this.AddXML.UseVisualStyleBackColor = true;
+            this.AddXML.Click += new System.EventHandler(this.AddXML_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 453);
+            this.Controls.Add(this.AddXML);
+            this.Controls.Add(this.CountTitle);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.AddCSV);
+            this.Name = "MainForm";
+            this.Text = "CSVXMLtoJSON";
+            this.Load += new System.EventHandler(this.MainForm_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddCSV;
+        private System.Windows.Forms.Label Count;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.Label CountTitle;
+        private System.Windows.Forms.Button AddXML;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Text;
+
+namespace Test2
+{
+    public partial class MainForm : Form
+    {
+        private List<string> processedCsvFiles;
+        private List<string> processedXmlFiles;
+        private List<UserData> userDataList;
+        public string countGenerateCountUser = "0";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            processedCsvFiles = new List<string>();
+            processedXmlFiles = new List<string>();
+            userDataList = new List<UserData>();
+        }
+
+        private void MainForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessCsvFile(string filePath)
+        {
+
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines.Skip(1))
+            {
+                var data = line.Split(';');
+                var userData = new UserData
+                {
+                    UserId = data[0],
+                    FirstName = data[1],
+                    LastName = data[2],
+                    Phone = data[3]
+                };
+                userDataList.Add(userData);
+            }
+
+            processedCsvFiles.Add(filePath);
+            GenerateLabelReport();
+        }
+
+        private void ProcessXmlFile(string filePath)
+        {
+            XElement xml = XElement.Load(filePath);
+            foreach (var card in xml.Elements("Card"))
+            {
+                var userData = new UserData
+                {
+                    UserId = card.Attribute("UserId").Value,
+                    Pan = card.Element("Pan").Value,
+                    ExpDate = card.Element("ExpDate").Value
+                };
+                userDataList.Add(userData);
+            }
+
+            processedXmlFiles.Add(filePath);
+            GenerateLabelReport();
+        }
+
+        private void GenerateLabelReport()
+        {
+            if (processedCsvFiles.Count > 0 && processedXmlFiles.Count > 0)
+            {
+                var countUser = userDataList.GroupBy(user => user.UserId)
+                                                                         .Where(group => group.Count() > 1)
+                                                                         .Count();
+                Count.Text = countUser.ToString();
+            }
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            {
+                if (processedCsvFiles.Count > 0 && processedXmlFiles.Count > 0)
+                {
+                    var combinedUserDataList = new List<UserData>();
+
+
+                    var groupedData = userDataList.GroupBy(user => user.UserId);
+
+                    foreach (var group in groupedData)
+                    {
+                        if (group.Count() < 2) continue;
+                        var userData = new UserData
+                        {
+                            UserId = group.Key,
+                            FirstName = group.Where(a => a.FirstName != null).Select(user => user.FirstName).FirstOrDefault(),
+                            LastName = group.Where(q => q.LastName != null).Select(user => user.LastName).FirstOrDefault(),
+                            Phone = group.Where(w => w.Phone != null).Select(user => user.Phone).FirstOrDefault(),
+                            Pan = group.Where(u => u.Pan != null).Select(user => user.Pan).FirstOrDefault(),
+                            ExpDate = group.Where(u => u.ExpDate != null).Select(user => user.ExpDate).FirstOrDefault()
+                        };
+
+                        combinedUserDataList.Add(userData);
+                    }
+                    string json = JsonConvert.SerializeObject(new { records = combinedUserDataList });
+                    
+                    SaveJsonToFile(json);
+                    
+                    MessageBox.Show($"Записей сформирована: {combinedUserDataList.Count}");
+
+
+
+                }
+            }
+        }
+        private void SaveJsonToFile(string json)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string outputPath = saveFileDialog.FileName;
+                    File.WriteAllText(outputPath, json);
+                }
+            }
+        }
+
+
+        private void AddCSV_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.csv";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                String path = dialog.FileName;
+                using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read), new UTF8Encoding())) // do anything you want, e.g. read it
+                {
+                    var nameArray = dialog.SafeFileName.Split('.');
+                    var type = nameArray[nameArray.Length - 1];
+                    switch (type)
+                    {
+                        case "csv": ProcessCsvFile(path); break;
+                    }
+
+                    var test = File.ReadAllLines(path);
+
+                }
+            }
+}
+
+        private void AddXML_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.xml;";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                String path = dialog.FileName;
+                using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read), new UTF8Encoding())) // do anything you want, e.g. read it
+                {
+                    var nameArray = dialog.SafeFileName.Split('.');
+                    var type = nameArray[nameArray.Length - 1];
+                    switch (type)
+                    {
+                        case "xml": ProcessXmlFile(path); break;
+                    }
+
+                    var test = File.ReadAllLines(path);
+
+                }
+            }
+            
+        }
+
+        public class UserData
+        {
+            public string UserId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Phone { get; set; }
+            public string Pan { get; set; }
+            public string ExpDate { get; set; }
+        }
+
+    }
+}
